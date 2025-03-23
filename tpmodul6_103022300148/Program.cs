74b6ebbd0f9f3733be6e52115fbce549602525d7
Yappier Albertus <yappier.albert1@gmail.com>
2025-03-23 21:18:36 +0700

    menambahkan class SayaTubeVideo

--- tpmodul6_103022300148/Program.cs
+++ tpmodul6_103022300148/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Security.Cryptography;
+class SayaTubeVideo {
+    Random rnd = new Random();
+    private int id, playCount;
+    private string title;
+
+    public SayaTubeVideo(String title) {
+        this.title = title;
+        id = rnd.Next(1,99999);
+        playCount = 0;
+    }
+    public void increasePlayCount(int x) {
+        playCount += x;
+    }
+    public void PrintVideoDetails() {
+        Console.WriteLine("ID : " + id);
+        Console.WriteLine("Judul Video : " + title);
+        Console.WriteLine("Jumlah View : " + playCount);
+    }
+}
+class Program
+{
+    public static void Main()
+    {
+        SayaTubeVideo vid = new SayaTubeVideo("Tutorial Design By Contract – Yappier Albertus Febriandi Krisna Putra");
+        vid.increasePlayCount(5);
+        vid.PrintVideoDetails();
+    }
+}
