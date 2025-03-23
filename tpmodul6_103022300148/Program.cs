918726a6947fcb27a70db4786430d07f520cc27d
Yappier Albertus <yappier.albert1@gmail.com>
2025-03-23 21:49:08 +0700

    implementasi design by contract

--- tpmodul6_103022300148/Program.cs
+++ tpmodul6_103022300148/Program.cs
@@ -1,16 +1,29 @@
-﻿using System.Security.Cryptography;
+﻿using System.Diagnostics.Contracts;
+using System.Security.Cryptography;
 class SayaTubeVideo {
     Random rnd = new Random();
     private int id, playCount;
     private string title;
-
     public SayaTubeVideo(String title) {
+        Contract.Requires(title.Length <= 100);
+        Contract.Requires(title != "");
         this.title = title;
-        id = rnd.Next(1,99999);
+        id = rnd.Next(10000,99999);
         playCount = 0;
     }
     public void increasePlayCount(int x) {
-        playCount += x;
+        Contract.Requires(x <= 10000000);
+        try
+        {
+            checked
+            {
+                playCount += x;
+            }
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine("Error Overflow");
+        }
     }
     public void PrintVideoDetails() {
         Console.WriteLine("ID : " + id);
@@ -18,12 +31,25 @@ class SayaTubeVideo {
         Console.WriteLine("Jumlah View : " + playCount);
     }
 }
+
 class Program
 {
     public static void Main()
     {
-        SayaTubeVideo vid = new SayaTubeVideo("Tutorial Design By Contract – Yappier Albertus Febriandi Krisna Putra");
-        vid.increasePlayCount(5);
-        vid.PrintVideoDetails();
+        try
+        {
+            SayaTubeVideo vid = new SayaTubeVideo("Tutorial Design By Contract – Yappier Albertus Febriandi Krisna Putra");
+            vid.PrintVideoDetails();
+            for (int i = 0; i < 220; i++)
+            {
+                vid.increasePlayCount(10000000);
+            }  
+            vid.PrintVideoDetails();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Terjadi kesalahan: " + e.Message);
+        }
+        
     }
 }
